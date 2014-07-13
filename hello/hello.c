96a1225a46b0420cdf364a442c89e7ebaa91726d
PalakPC <no.1palak@gmail.com>
2014-07-13 23:06:11 +0530

    Improvements

--- hello/hello.c
+++ hello/hello.c
@@ -1,21 +1,21 @@
-//Kernel module to print "Hello World!" to log.
+/*Kernel module to print "Hello World!" to log.*/
 
-//Undefining because otherwise code gives redefinition warning on compilation.
+/*Undefining because otherwise code gives redefinition warning on compilation.*/
 #undef __KERNEL__
 #undef MODULE
 
-//Defining __KERNEL__ and MODULE allows us to access kernel-level code not usually available to userspace programs.
+/*Defining __KERNEL__ and MODULE allows us to access kernel-level code not usually available to userspace programs.*/
 #define __KERNEL__
 #define MODULE
  
-//Linux Kernel/LKM headers: module.h is needed by all modules and kernel.h is needed for KERN_INFO.
-#include <linux/module.h>	//Included for all kernel modules.
-#include <linux/kernel.h>	//Included for KERN_INFO.
-#include <linux/init.h>		//Included for __init and __exit macros.
+/*Linux Kernel/LKM headers: module.h is needed by all modules and kernel.h is needed for KERN_INFO.*/
+#include <linux/module.h>	/*Included for all kernel modules.*/
+#include <linux/kernel.h>	/*Included for KERN_INFO.*/
+#include <linux/init.h>		/*Included for __init and __exit macros.*/
  
 static int __init hello_init (void) {
 	printk (KERN_INFO "Hello world!\n");
-	return 0;	//Non-zero return means that the module couldn't be loaded.
+	return 0;	/*Non-zero return means that the module couldn't be loaded.*/
 }
  
 static void __exit hello_cleanup (void) {
